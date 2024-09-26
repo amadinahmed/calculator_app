dbec6221d7ece65d85864792abc3e203e4a4df7b
Amadin Ahmed <48179347+amadinahmed@users.noreply.github.com>
2024-09-26 23:10:33 +0000

    functional calculator app

---
+++ scripts.js
@@ -0,0 +1,60 @@
+// Simple Calculator JavaScript
+
+// Get the display element
+let display = document.getElementById('display');
+
+// Variables to store our numbers and operation
+let firstNumber = '';
+let secondNumber = '';
+let operation = '';
+
+// Function to append a number to the display
+function appendNumber(num) {
+    if (operation === '') {
+        firstNumber += num;
+        display.value = firstNumber;
+    } else {
+        secondNumber += num;
+        display.value = secondNumber;
+    }
+}
+
+// Function to set the operation
+function setOperation(op) {
+    operation = op;
+    display.value = '';
+}
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+    firstNumber = '';
+    secondNumber = '';
+    operation = '';
+}
+
+// Function to calculate the result
+function calculate() {
+    let result;
+    let num1 = parseFloat(firstNumber);
+    let num2 = parseFloat(secondNumber);
+
+    if (operation === '+') {
+        result = num1 + num2;
+    } else if (operation === '-') {
+        result = num1 - num2;
+    } else if (operation === '*') {
+        result = num1 * num2;
+    } else if (operation === '/') {
+        if (num2 !== 0) {
+            result = num1 / num2;
+        } else {
+            result = 'Error';
+        }
+    }
+
+    display.value = result;
+    firstNumber = result.toString();
+    secondNumber = '';
+    operation = '';
+}
